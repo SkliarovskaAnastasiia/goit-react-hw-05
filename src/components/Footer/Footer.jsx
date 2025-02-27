e98afd41abb51c5d657e10fe1700d3516ceed064
Anastasiia Skliarovska <skliarovskaa@gmail.com>
2025-02-27 15:35:24 +0200

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,11 @@ import css from './Footer.module.css';
 export default function Footer() {
   return (
     <footer className={css.footer}>
-      <a href="https://www.themoviedb.org/" className={css.footerLink}>
+      <a
+        href="https://www.themoviedb.org/"
+        target="_blank"
+        className={css.footerLink}
+      >
         TMDB API
       </a>
     </footer>
